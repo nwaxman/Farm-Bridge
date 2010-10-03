d3a19776eb95071a416d0c5d2b8608df8c2062b4
Rune Skjoldborg Madsen <rune@runemadsen.com>
2010-10-03 11:17:57 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farmbridge::Application.config.secret_token = 'cda432d80c8615232a84aad6667d7a40672b77896d84ead3b1b8294e319c539daceb3423ba423e64a056023f58a4e2dea0bdd0cc4b7b4378195c5b802c66cf48'
