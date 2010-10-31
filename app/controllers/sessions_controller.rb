c7826673f8b39e4dfba1c5a243662287fc19806e
Julio Terra <julioterra@gmail.com>
2010-10-31 15:18:06 -0400

    updated csa manager sign-up process, cleaning up broken links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
 
-      redirect_back_or_default('/dashboard', :notice => "Logged in successfully")
+      redirect_back_or_default('/', :notice => "Logged in successfully")
     else
       note_failed_signin
       @login       = params[:login]
