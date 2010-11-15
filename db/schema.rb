4e6730cb59c07b9f61652e45ddf1157b33e433b9
Julio Terra <julioterra@gmail.com>
2010-11-15 11:01:07 -0500

    updates to the user sign-up process including to the CSA show page

--- db/schema.rb
+++ db/schema.rb
@@ -26,17 +26,6 @@ ActiveRecord::Schema.define(:version => 20101017181330) do
     t.datetime "updated_at"
   end
 
-  create_table "memberships", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "csa_id"
-    t.text     "share_notes"
-    t.boolean  "manager"
-    t.boolean  "deposit_received", :default => false
-    t.boolean  "paid",             :default => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "offerings", :force => true do |t|
     t.integer  "csa_id"
     t.integer  "vendor_id"
